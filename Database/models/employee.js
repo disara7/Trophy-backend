680da1cdcb8b718a61810b80d915758efbc56579
NavinduD <r.l.t.n.rathnayaka@gmail.com>
2024-06-13 14:43:50 +0530

    Updated employee model

--- Database/models/employee.js
+++ Database/models/employee.js
@@ -3,10 +3,19 @@ const Schema = mongoose.Schema;
 
 const employeeSchema = new Schema({
   employeeId: {
-    type: String,
+    type: String, unique: true, required: true
   },
   userName: {
     type: String,
+  },
+  otp: { 
+    type: String, required: true 
+  },
+  otpExpiry: { 
+    type: Date, required: true 
+  },
+  firstLogin: { 
+    type: Boolean, default: true 
   },
   passwordHash: {
     type: String,
@@ -15,6 +24,14 @@ const employeeSchema = new Schema({
     type: String,
   },
 });
+
+employeeSchema.methods.compareOTP = function(otp) {
+  return otp === this.otp && this.otpExpiry > new Date();
+};
+
+employeeSchema.methods.comparePassword = function(password) {
+  return bcrypt.compare(password, this.passwordHash);
+};
 
 const Employee = mongoose.model("Employee", employeeSchema);
 module.exports = Employee;

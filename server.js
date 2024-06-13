16fccfe19cde27974c4f1f4f4975b2cf0dcf96cc
NavinduD <r.l.t.n.rathnayaka@gmail.com>
2024-06-13 17:48:22 +0530

    fixed

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const connect = async () => {
     await mongoose.connect(uri);
     console.log("Database conncted!");
   } catch (error) {
-    console.log(error.message());
+    console.log(error.message);
   }
 };
 

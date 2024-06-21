f95f0575cf85fa239887f64e3ff2d2aa8a41d213
DushyanthaAT <dushyantha721@gmail.com>
2024-06-21 12:22:43 +0530

    HashError

--- Database/controller/employee_controller.js
+++ Database/controller/employee_controller.js
@@ -12,10 +12,13 @@ const getEmployee = (req, res, next) => {
 
 const addEmployee = (req, res, next) => {
   const employee = new Employee({
-    employeeId: req.body.employeeid,
+    employeeId: req.body.employeeId,
     userName: req.body.userName,
     passwordHash: req.body.passwordHash,
     employeeName: req.body.employeeName,
+    otp: req.body.otp,
+    otpExpiry: req.body.otpExpiry,
+    firstLogin: req.body.firstLogin,
   });
   employee
     .save()
--- Database/models/employee.js
+++ Database/models/employee.js
@@ -3,22 +3,24 @@ const Schema = mongoose.Schema;
 
 const employeeSchema = new Schema({
   employeeId: {
-    type: String, unique: true, required: true
+    type: String,
+    unique: true,
+    required: true,
   },
   username: {
     type: String,
   },
-  otp: { 
-    type: String, required: true 
-  },
-  otpExpiry: { 
-    type: Date, required: true 
-  },
-  firstLogin: { 
-    type: Boolean, default: true 
-  },
   passwordHash: {
     type: String,
+  },
+  otp: {
+    type: String, required: true
+  },
+  otpExpiry: {
+    type: Date, required: true
+  },
+  firstLogin: {
+    type: Boolean, default: true
   },
   employeeName: {
     type: String,
@@ -30,24 +32,24 @@ employeeSchema.methods.compareOTP = async function(otp) {
   return isOtpValid && this.otpExpiry > new Date();
 };
 
-employeeSchema.methods.comparePassword = function(password) {
-  return bcrypt.compare(password, this.passwordHash);
-};
+// employeeSchema.methods.comparePassword = function(password) {
+//   return bcrypt.compare(password, this.passwordHash);
+// };
 
-employeeSchema.pre('save', async function(next) {
-  //Hash password
-  if (this.isModified('passwordHash')) {
-    const salt = await bcrypt.genSalt(10);
-    this.passwordHash = await bcrypt.hash(this.passwordHash, salt);
-  }
-  //Hash otp
-  if (this.isModified('otp')) {
-    const salt = await bcrypt.genSalt(10);
-    this.otp = await bcrypt.hash(this.otp, salt);
-  }
+// employeeSchema.pre('save', async function(next) {
+//   //Hash password
+//   if (this.isModified('passwordHash')) {
+//     const salt = await bcrypt.genSalt(10);
+//     this.passwordHash = await bcrypt.hash(this.passwordHash, salt);
+//   }
+//   //Hash otp
+//   if (this.isModified('otp')) {
+//     const salt = await bcrypt.genSalt(10);
+//     this.otp = await bcrypt.hash(this.otp, salt);
+//   }
 
-  next();
-});
+//   next();
+// });
 
 const Employee = mongoose.model("Employee", employeeSchema);
 module.exports = Employee;

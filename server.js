377dd1d2467dee2e01e6348feca433d143614280
Minuri <92873876+minuri12@users.noreply.github.com>
2024-07-02 12:47:10 +0530

    Port change

--- server.js
+++ server.js
@@ -1,46 +1,48 @@
+// Load environment variables from .env file
 require("dotenv").config();
+
 const express = require("express");
 const bodyParser = require("body-parser");
-const authRoutes = require("./routes/auth");
-
-const PORT = 80;
-const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-const router = require("./Database/router");
 
+const authRoutes = require("./routes/auth");
+const activityRoutes = require("./routes/activityRoutes");
+
+const PORT = 3005;
+const app = express();
+const uri = process.env.MONGO_URI;
+
+// Middleware
 app.use(cors());
 app.use(express.json());
-
 app.use(bodyParser.json());
-app.use("/auth", authRoutes);
 
+// Routes
+app.use("/auth", authRoutes);
+app.use("/api", activityRoutes);
+
+// Root Route
 app.get("/", (req, res) => {
   res.send("Hello");
 });
 
+// Start Server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-const uri = process.env.MONGO_URI;
 
-// Activity Routes
-const activityRoutes = require("./routes/activityRoutes");
-app.use("/api", activityRoutes);
-
-const connect = async () => {
+// Connect to MongoDB
+const connectDB = async () => {
   try {
-    await mongoose.connect(uri);
-    console.log("Database conncted!");
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Database connected!");
   } catch (error) {
-    console.log(error.message);
+    console.log("Error connecting to database:", error.message);
   }
 };
 
-connect();
-
-// const server = app.listen("3001", "localhost", () =>
-//   console.log("Server is running")
-// );
-
-//app.use("/api", router);
+connectDB();

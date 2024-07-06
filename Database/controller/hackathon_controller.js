fc13488dedb128a7aac5297d8dc58e13448e599d
Minuri <92873876+minuri12@users.noreply.github.com>
2024-07-06 10:54:21 +0530

    Update Hackathons

--- Database/controller/hackathon_controller.js
+++ Database/controller/hackathon_controller.js
@@ -2,8 +2,8 @@ const Hackathon = require("../models/hackathon");
 
 const getHackathons = async (req, res) => {
   try {
-    const hackathons = await Hackathon.find();
-    res.status(200).json(hackathons);
+    const Hackathons = await Hackathon.find();
+    res.status(200).json(Hackathons);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
